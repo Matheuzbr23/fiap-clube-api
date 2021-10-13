67faa0a7efa9a71471b644f091dfba173b1ee08e
Shiguemori <shiguemori@hotmail.com>
2021-10-13 20:54:16 -0300

    make it better

--- setup/settings.py
+++ setup/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = [
 
 # Application definition
 INSTALLED_APPS = [
-    "corsheaders",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,7 +44,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
