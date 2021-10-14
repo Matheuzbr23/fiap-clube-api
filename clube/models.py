657b3432cf21bfa7ad6f102e92e731f11b6f5255
Shiguemori <shiguemori@hotmail.com>
2021-10-13 22:04:52 -0300

    ajustes migrations

--- clube/models.py
+++ clube/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 import sys
 
+
 class Usuario(models.Model):
     nome = models.CharField(max_length=30)
     email = models.EmailField(blank=False, max_length=100, unique=True)
     senha = models.CharField(max_length=30)
     ativo = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.nome
 
 
-class Clube (models.Model):
+class Clube(models.Model):
     TIPO = (
         ('L', 'Livro'),
         ('F', 'Filme'),
@@ -22,6 +24,7 @@ class Clube (models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.CharField(max_length=100)
     tipo = models.CharField(max_length=1, choices=TIPO, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.nome
@@ -29,14 +32,17 @@ class Clube (models.Model):
 
 class Storie(models.Model):
     clube = models.ForeignKey(Clube, on_delete=models.CASCADE)
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)    
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     data = models.DateField()
     texto = models.CharField(max_length=10000)
-    
+    created_at = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return self.texto
 
+
 class UsuarioClube(models.Model):
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)    
-    clube = models.ForeignKey(Clube, on_delete=models.CASCADE)   
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    clube = models.ForeignKey(Clube, on_delete=models.CASCADE)
     ativo = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
--- clube/serializer.py
+++ clube/serializer.py
@@ -1,45 +1,55 @@
-from django.core.exceptions import ValidationError
 from rest_framework import serializers
+
 from clube.models import Clube, Usuario, Storie, UsuarioClube
+
 
 class ClubeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Clube
         fields = ['id', 'nome', 'descricao', 'tipo']
 
+
 class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Usuario
         fields = '__all__'
+
     def validate_nome(self, nome):
-        if not nome.replace(' ','').isalpha():
+        if not nome.replace(' ', '').isalpha():
             raise serializers.ValidationError("Não é permitido a inclusão de números do nome do usuário.")
         return nome
+
     def validate_senha(self, senha):
         if len(senha) < 8:
             raise serializers.ValidationError("A senha deve conter 8 ou mais digitos")
         return senha
+
 
 class StorieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Storie
         fields = '__all__'
 
+
 class UsuarioClubeSerializer(serializers.ModelSerializer):
     class Meta:
         model = UsuarioClube
         fields = '__all__'
 
+
 class ListaClubesUsuarioSerializer(serializers.ModelSerializer):
     clube_id = serializers.ReadOnlyField(source='clube.id')
     clube_nome = serializers.ReadOnlyField(source='clube.nome')
+
     class Meta:
         model = UsuarioClube
         fields = ['clube_id', 'clube_nome', 'ativo']
 
+
 class ListaStoriesClubeSerializer(serializers.ModelSerializer):
     usuario_id = serializers.ReadOnlyField(source='usuario.id')
     usuario_nome = serializers.ReadOnlyField(source='usuario.nome')
+
     class Meta:
         model = Storie
         fields = ['id', 'data', 'texto', 'usuario_id', 'usuario_nome']
